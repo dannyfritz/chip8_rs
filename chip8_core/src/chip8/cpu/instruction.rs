74ea8ee6a9cab74ac7c35f088d548fa05b9b765c
Danny Fritz <dannyfritz@gmail.com>
2018-07-29 14:00:15 -0600

    move def of get_opcode macro

--- chip8_core/src/chip8/cpu/instruction.rs
+++ chip8_core/src/chip8/cpu/instruction.rs
@@ -1,20 +1,6 @@
 use chip8::{Address, Register};
 use std::fmt;
 
-macro_rules! no_opcode {
-    ($x: expr) => {
-        panic!(format!("No OpCode for 0x{:04X} yet!", $x));
-    };
-}
-
-macro_rules! get_opcode {
-    ($op_code:expr, $mask:expr, $output:expr, $value: expr) => {
-        if $value & $mask == $op_code {
-            return $output
-        }
-    };
-}
-
 pub enum OpCode {
     Set(Register, u8),
     Copy(Register, Register),
@@ -105,40 +91,52 @@ impl Instruction {
         Instruction { value }
     }
     pub fn decode(&self) -> OpCode {
-        get_opcode!(0x00E0, 0xFFFF, OpCode::ClearScreen(), self.value);
-        get_opcode!(0x00EE, 0xFFFF, OpCode::Return(), self.value);
-        get_opcode!(0x1000, 0xF000, OpCode::Jmp(self.get_address()), self.value);
-        get_opcode!(0x2000, 0xF000, OpCode::Call(self.get_address()), self.value);
-        get_opcode!(0x3000, 0xF000, OpCode::Jeq(self.get_vx(), self.get_8bconst()), self.value);
-        get_opcode!(0x4000, 0xF000, OpCode::Jneq(self.get_vx(), self.get_8bconst()), self.value);
-        get_opcode!(0x5000, 0xF00F, OpCode::JeqVy(self.get_vx(), self.get_vy()), self.value);
-        get_opcode!(0x6000, 0xF000, OpCode::Set(self.get_vx(), self.get_8bconst()), self.value);
-        get_opcode!(0x7000, 0xF000, OpCode::Add(self.get_vx(), self.get_8bconst()), self.value);
-        get_opcode!(0x8000, 0xF00F, OpCode::Copy(self.get_vx(), self.get_vy()), self.value);
-        get_opcode!(0x8001, 0xF00F, OpCode::Or(self.get_vx(), self.get_vy()), self.value);
-        get_opcode!(0x8002, 0xF00F, OpCode::And(self.get_vx(), self.get_vy()), self.value);
-        get_opcode!(0x8003, 0xF00F, OpCode::Xor(self.get_vx(), self.get_vy()), self.value);
-        get_opcode!(0x8004, 0xF00F, OpCode::AddVy(self.get_vx(), self.get_vy()), self.value);
-        get_opcode!(0x8005, 0xF00F, OpCode::SubVy(self.get_vx(), self.get_vy()), self.value);
-        get_opcode!(0x8006, 0xF00F, OpCode::ShiftRight(self.get_vx(), self.get_vy()), self.value);
-        get_opcode!(0x8007, 0xF00F, OpCode::SubVx(self.get_vx(), self.get_vy()), self.value);
-        get_opcode!(0x8008, 0xF00F, OpCode::ShiftLeft(self.get_vx(), self.get_vy()), self.value);
-        get_opcode!(0x9000, 0xF000, OpCode::JneqVy(self.get_vx(), self.get_vy()), self.value);
-        get_opcode!(0xA000, 0xF000, OpCode::SetI(self.get_address()), self.value);
-        get_opcode!(0xB000, 0xF000, OpCode::JmpV0(self.get_address()), self.value);
-        get_opcode!(0xC000, 0xF000, OpCode::Random(self.get_vx(), self.get_8bconst()), self.value);
-        get_opcode!(0xD000, 0xF000, OpCode::DrawSprite(self.get_vx(), self.get_vy(), self.get_4bconst()), self.value);
-        get_opcode!(0xE09E, 0xF0FF, OpCode::JmpK(self.get_vx()), self.value);
-        get_opcode!(0xE0A1, 0xF0FF, OpCode::JmpNK(self.get_vx()), self.value);
-        get_opcode!(0xF00A, 0xF0FF, OpCode::WaitForKey(self.get_vx()), self.value);
-        get_opcode!(0xF007, 0xF0FF, OpCode::LdDelayTimer(self.get_vx()), self.value);
-        get_opcode!(0xF015, 0xF0FF, OpCode::SetDelayTimer(self.get_vx()), self.value);
-        get_opcode!(0xF018, 0xF0FF, OpCode::SetSoundTimer(self.get_vx()), self.value);
-        get_opcode!(0xF01E, 0xF0FF, OpCode::AddIVx(self.get_vx()), self.value);
-        get_opcode!(0xF029, 0xF0FF, OpCode::Font(self.get_vx()), self.value);
-        get_opcode!(0xF033, 0xF0FF, OpCode::BCD(self.get_vx()), self.value);
-        get_opcode!(0xF055, 0xF0FF, OpCode::Store(self.get_vx()), self.value);
-        get_opcode!(0xF065, 0xF0FF, OpCode::Load(self.get_vx()), self.value);
+        macro_rules! no_opcode {
+            ($x: expr) => {
+                panic!(format!("No OpCode for 0x{:04X} yet!", $x));
+            };
+        }
+        macro_rules! get_opcode {
+            ($op_code:expr, $mask:expr, $output:expr) => {
+                if self.value & $mask == $op_code {
+                    return $output
+                }
+            };
+        }
+        get_opcode!(0x00E0, 0xFFFF, OpCode::ClearScreen());
+        get_opcode!(0x00EE, 0xFFFF, OpCode::Return());
+        get_opcode!(0x1000, 0xF000, OpCode::Jmp(self.get_address()));
+        get_opcode!(0x2000, 0xF000, OpCode::Call(self.get_address()));
+        get_opcode!(0x3000, 0xF000, OpCode::Jeq(self.get_vx(), self.get_8bconst()));
+        get_opcode!(0x4000, 0xF000, OpCode::Jneq(self.get_vx(), self.get_8bconst()));
+        get_opcode!(0x5000, 0xF00F, OpCode::JeqVy(self.get_vx(), self.get_vy()));
+        get_opcode!(0x6000, 0xF000, OpCode::Set(self.get_vx(), self.get_8bconst()));
+        get_opcode!(0x7000, 0xF000, OpCode::Add(self.get_vx(), self.get_8bconst()));
+        get_opcode!(0x8000, 0xF00F, OpCode::Copy(self.get_vx(), self.get_vy()));
+        get_opcode!(0x8001, 0xF00F, OpCode::Or(self.get_vx(), self.get_vy()));
+        get_opcode!(0x8002, 0xF00F, OpCode::And(self.get_vx(), self.get_vy()));
+        get_opcode!(0x8003, 0xF00F, OpCode::Xor(self.get_vx(), self.get_vy()));
+        get_opcode!(0x8004, 0xF00F, OpCode::AddVy(self.get_vx(), self.get_vy()));
+        get_opcode!(0x8005, 0xF00F, OpCode::SubVy(self.get_vx(), self.get_vy()));
+        get_opcode!(0x8006, 0xF00F, OpCode::ShiftRight(self.get_vx(), self.get_vy()));
+        get_opcode!(0x8007, 0xF00F, OpCode::SubVx(self.get_vx(), self.get_vy()));
+        get_opcode!(0x8008, 0xF00F, OpCode::ShiftLeft(self.get_vx(), self.get_vy()));
+        get_opcode!(0x9000, 0xF000, OpCode::JneqVy(self.get_vx(), self.get_vy()));
+        get_opcode!(0xA000, 0xF000, OpCode::SetI(self.get_address()));
+        get_opcode!(0xB000, 0xF000, OpCode::JmpV0(self.get_address()));
+        get_opcode!(0xC000, 0xF000, OpCode::Random(self.get_vx(), self.get_8bconst()));
+        get_opcode!(0xD000, 0xF000, OpCode::DrawSprite(self.get_vx(), self.get_vy(), self.get_4bconst()));
+        get_opcode!(0xE09E, 0xF0FF, OpCode::JmpK(self.get_vx()));
+        get_opcode!(0xE0A1, 0xF0FF, OpCode::JmpNK(self.get_vx()));
+        get_opcode!(0xF00A, 0xF0FF, OpCode::WaitForKey(self.get_vx()));
+        get_opcode!(0xF007, 0xF0FF, OpCode::LdDelayTimer(self.get_vx()));
+        get_opcode!(0xF015, 0xF0FF, OpCode::SetDelayTimer(self.get_vx()));
+        get_opcode!(0xF018, 0xF0FF, OpCode::SetSoundTimer(self.get_vx()));
+        get_opcode!(0xF01E, 0xF0FF, OpCode::AddIVx(self.get_vx()));
+        get_opcode!(0xF029, 0xF0FF, OpCode::Font(self.get_vx()));
+        get_opcode!(0xF033, 0xF0FF, OpCode::BCD(self.get_vx()));
+        get_opcode!(0xF055, 0xF0FF, OpCode::Store(self.get_vx()));
+        get_opcode!(0xF065, 0xF0FF, OpCode::Load(self.get_vx()));
         no_opcode!(self.value);
     }
     pub fn get_address(&self) -> Address {
